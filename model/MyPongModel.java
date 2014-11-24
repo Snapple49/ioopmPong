e17ded999cd2f07b5e41391849d56b1650b3c514
Snapple49 <oliver-dot-stein@hotmail.com>
2014-11-24 15:01:08 +0100

    Lagt till score() i MyPongModel

--- model/MyPongModel.java
+++ model/MyPongModel.java
@@ -9,20 +9,25 @@ public class MyPongModel implements PongModel {
 	private int rightBarHeight;
 	private int leftPos;
 	private int rightPos;
-	private String leftScore;
-	private String rightScore;
+	
+	private int leftScore;
+	private int rightScore;
+	//private int[] scoreArray = {Integer.parseInt(leftScore), Integer.parseInt(rightScore)};
+
 	private final Dimension fieldSize;
 	private Ball ball;
+	private final Point center; 
 
 	public MyPongModel(String leftPlayer, String rightPlayer) {
 		this.leftBarHeight = 150;
 		this.rightBarHeight = 150;
-		this.leftPos = 400;
-		this.rightPos = 400;
-		this.leftScore = "0";
-		this.rightScore = "0";
 		this.fieldSize = new Dimension(1200,800);
-		this.ball = new Ball(600,400, new Point(-8,-8));
+		this.center = new Point(((fieldSize.width)/2), ((fieldSize.height)/2));
+		this.leftPos = center.y;
+		this.rightPos = center.y;
+		this.leftScore = 0;
+		this.rightScore = 0;
+		this.ball = new Ball(center.x,center.y, new Point(15,0));
 
 	}
 
@@ -35,7 +40,7 @@ public class MyPongModel implements PongModel {
 	 */
 	public void compute(Set<Input> input, long delta_t) {
 		this.ball.move();
-		this.checkCollision();
+		this.checkCollision(BarKey.LEFT, BarKey.RIGHT);
 		for(Input i : input){
 			switch (i.key) {
 			case RIGHT:
@@ -58,94 +63,109 @@ public class MyPongModel implements PongModel {
 
 	}
 
-	public void checkCollision(BarKey bar){
+	public void score(boolean left){
+		this.ball.setLocation(this.center);
+		if (left) {
+			rightScore++;
+		}else{
+			leftScore++;
+		}
+		this.leftPos = center.y;
+		this.rightPos = center.y;
+	}
+	
+	public void checkCollision(BarKey left, BarKey right){
 		if (this.ball.y >= fieldSize.height || this.ball.y <= 0){
 			this.ball.changeDirection(false);
 		}
-		if (this.ball.x <= 0){
-			if(this.ball.y < this.getBarPos(bar)-(leftBarHeight/2) && this.ball.y > this.getBarPos(bar)+(leftBarHeight/2)){				
-				this.ball.changeDirection(true);
-			}
-		}
 		if (this.ball.x >= this.fieldSize.width){
-			if (this.ball.y < this.getBarPos(bar)-(rightBarHeight/2) && this.ball.y > this.getBarPos(bar)+(rightBarHeight/2)){
-				
+			if(this.ball.y > this.getBarPos(right)-(leftBarHeight/2) && this.ball.y < this.getBarPos(right)+(leftBarHeight/2)){				
+				this.ball.changeDirection(true);
+			}else{
+				score(true);
+			}
+		}
+		if (this.ball.x <= 0){
+			if (this.ball.y > this.getBarPos(left)-(rightBarHeight/2) && this.ball.y < this.getBarPos(left)+(rightBarHeight/2)){
+				this.ball.changeDirection(true);
+			}else{				
+				score(false);
 			}
 		}
 	}
 
 
-		/**
-		 * getters that take a BarKey LEFT or RIGHT
-		 * and return positions of the various items on the board
-		 */
+	/**
+	 * getters that take a BarKey LEFT or RIGHT
+	 * and return positions of the various items on the board
+	 */
 
-		public int getBarHeight(BarKey k) {
-			switch (k){
-			case LEFT:
-				return leftBarHeight;
-			case RIGHT:
-				return rightBarHeight;
-			default:
-				return 0;
-			}
+	public int getBarHeight(BarKey k) {
+		switch (k){
+		case LEFT:
+			return leftBarHeight;
+		case RIGHT:
+			return rightBarHeight;
+		default:
+			return 0;
 		}
+	}
 
 
-		/**
-		 * Will output information about the state of the game to be
-		 * displayed to the players
-		 */
-		public String getMessage() {
-			return "Det här är ett kul spel!";
-		}
-
-
-
-		/**
-		 * getters that take a BarKey LEFT or RIGHT
-		 * and return positions of the various items on the board
-		 */
-		public int getBarPos(BarKey k) {
-			switch (k) {
-			case LEFT: 
-				return this.leftPos;
-			case RIGHT:
-				return this.rightPos;
-			default:
-				return 0;
-			}
-
-		}
-		public Point getBallPos() {
-			return (Point) ball;
-
-		}
+	/**
+	 * Will output information about the state of the game to be
+	 * displayed to the players
+	 */
+	public String getMessage() {
+		return "Det här är ett kul spel!";
+	}
 
 
 
-		/**
-		 * getter for the scores.
-		 */
-		public String getScore(BarKey k) {
-			switch (k) {
-			case LEFT: 
-				return this.leftScore;
-			case RIGHT:
-				return this.rightScore;
-			default:
-				return "";
-
-			}
-
-		}
-
-		/**
-		 * a valid implementation of the model will keep the field size
-		 * will remain constant forever.
-		 */
-		public Dimension getFieldSize() {
-			return this.fieldSize;
+	/**
+	 * getters that take a BarKey LEFT or RIGHT
+	 * and return positions of the various items on the board
+	 */
+	public int getBarPos(BarKey k) {
+		switch (k) {
+		case LEFT: 
+			return this.leftPos;
+		case RIGHT:
+			return this.rightPos;
+		default:
+			return 0;
 		}
 
 	}
+	public Point getBallPos() {
+		return (Point) ball;
+
+	}
+
+
+
+	/**
+	 * getter for the scores.
+	 */
+	public String getScore(BarKey k) {
+		switch (k) {
+		case LEFT: 
+			return ((Integer)this.leftScore).toString();
+		case RIGHT:
+			return ((Integer)this.rightScore).toString();
+		default:
+			return "";
+
+		}
+
+	}
+
+	/**
+	 * a valid implementation of the model will keep the field size
+	 * will remain constant forever.
+	 */
+	public Dimension getFieldSize() {
+		return this.fieldSize;
+	}
+
+}
