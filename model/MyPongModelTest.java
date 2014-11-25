d78cda9a4ab37c4a4a07af1a28375e7bcf5c46c6
Nallexander <alexander.e.lind@gmail.com>
2014-11-25 15:25:35 +0100

    mer MyPongModel-tester samt lite ändringar MyPongModel

--- model/MyPongModelTest.java
+++ model/MyPongModelTest.java
@@ -38,7 +38,7 @@ public class MyPongModelTest {
 		
 		testModel.compute(testInput, 1);
 		assertTrue(testModel.getBarPos(BarKey.LEFT) == 399);
-		System.out.println(testModel.getBarPos(BarKey.RIGHT));
+		
 		assertTrue(testModel.getBarPos(BarKey.RIGHT) == 401);
 		testModel.compute(testInput2, 1);
 		assertTrue(testModel.getBarPos(BarKey.LEFT) == 400);
@@ -66,8 +66,67 @@ public class MyPongModelTest {
 		MyPongModel testModel = new MyPongModel("hej", "hoj", new Point(1191, 500), new Point(5,0));
 		testModel.compute(testInput, 1);
 		testModel.compute(testInput, 1);
-		assertTrue(testModel.getScore(BarKey.LEFT).equals("1"));
+		assertTrue(testModel.getScore(BarKey.LEFT).equals("1"));	
+	}
+	@Test
+	public void checkCollissionTopBottomTest() {
+		MyPongModel testModel = new MyPongModel("hej", "hoj", new Point(50, 5), new Point(0,-5));
+		
+		Set<Input> testInput = new HashSet<Input>();
+		Input testBarRightDown = new Input(BarKey.RIGHT, Dir.DOWN);
+		testInput.add(testBarRightDown);
+		
+		testModel.compute(testInput, 1);
+		assertTrue(testModel.getBallPos().y == 0);
+		testModel.compute(testInput, 1);
+		assertTrue(testModel.getBallPos().y == 5);
+		MyPongModel testModel2 = new MyPongModel("hej", "hoj", new Point(50, 795), new Point(0,5));
+		testModel2.compute(testInput, 1);
+		assertTrue(testModel2.getBallPos().y == 800);
+		testModel2.compute(testInput, 1);
+		assertTrue(testModel2.getBallPos().y == 795);
+	}
+	@Test
+	public void testCheckCollisionBarsTest() {
+		MyPongModel testModel = new MyPongModel("hej", "hoj", new Point(1195, 400), new Point(5,0));
+		
+		Set<Input> testInput = new HashSet<Input>();
+		Input testBarRightDown = new Input(BarKey.RIGHT, Dir.DOWN);
+		testInput.add(testBarRightDown);
+		
+		testModel.compute(testInput, 1);
+		assertTrue(testModel.getBallPos().x == 1200);
+		
+		testModel.compute(testInput, 1);
+		assertTrue(testModel.getBallPos().x == 1195);
+		
+		MyPongModel testModel2 = new MyPongModel("hej", "hoj", new Point(5, 400), new Point(-5,0));
+
+		testModel2.compute(testInput, 1);
+		assertTrue(testModel2.getBallPos().x == 0);
+		
+		testModel2.compute(testInput, 1);
+		assertTrue(testModel2.getBallPos().x == 5);
 		
 		
 	}
+	@Test
+	public void checkCollisionNoBarTest() {
+		MyPongModel testModel = new MyPongModel("hej", "hoj", new Point(1195, 0), new Point(5,0));
+		
+		Set<Input> testInput = new HashSet<Input>();
+		Input testBarRightDown = new Input(BarKey.RIGHT, Dir.DOWN);
+		testInput.add(testBarRightDown);
+		
+		testModel.compute(testInput, 1);
+		assertTrue(testModel.getBallPos().x == 600);
+		
+		MyPongModel testModel2 = new MyPongModel("hej", "hoj", new Point(5, 0), new Point(-5,0));
+		
+		testModel2.compute(testInput, 1);
+		assertTrue(testModel2.getBallPos().x == 600);
+		
+		
+	}
+	
 }
